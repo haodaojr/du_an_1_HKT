6c090110d7d499ad679b2c79c208998b2bd7a66e
tuanhoccode <tuanndph53054@gmail.com>
2024-11-13 10:43:02 +0700

    file moi

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Xin chào bạn</h1>
-</body>
-</html>
+<?php 
+
+// Require file Common
+require_once './commons/env.php'; // Khai báo biến môi trường
+require_once './commons/function.php'; // Hàm hỗ trợ
+
+// Require toàn bộ file Controllers
+require_once './controllers/HomeController.php';
+
+// Require toàn bộ file Models
+
+// Route
+$act = $_GET['act'] ?? '/';
+
+// Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
+
+match ($act) {
+    // Trang chủ
+    '/'                 => (new HomeController())->index(),
+};
